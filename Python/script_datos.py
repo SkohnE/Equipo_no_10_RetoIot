3377610c9cad2a29449597034bd489c2ba87bcae
S. Kohn <s.kohne99@gmail.com>
2020-11-10 00:20:52 -0600

    modificación 5000 registros

--- Python/script_datos.py
+++ Python/script_datos.py
@@ -11,7 +11,6 @@ def add_user(name, age):
 def add_register(user, types, register):
   date = datetime.now()
   query_data = (user, types, register, date, )
-  print(user, types, register, date)
   query = (f"INSERT INTO registros (id_persona, tipo_registro, registro, fecha) values (%s,%s,%s,%s);")
   cursor.execute(query, query_data)
   cnx.commit()
@@ -20,31 +19,41 @@ try:
   cnx = mysql.connector.connect(user='root', password='kokoloco', host='127.0.0.1', database='iot_reto', auth_plugin='mysql_native_password')
   cursor = cnx.cursor()
   
-  while(True):
-    print(" \n-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·- \n")
-    print("1. Agrega un usuario.")
-    print("2. Agrega un Registro.")
-    print("3. Salir.")
-    opcion = int(input("Escoge la opción: "))
-    if(opcion == 1):
-      name = input("Nombre: ")
-      age = int(input("Edad: "))
-      add_user(name, age)
+  # while(True):
+  #   print(" \n-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·-·- \n")
+  #   print("1. Agrega un usuario.")
+  #   print("2. Agrega un Registro.")
+  #   print("3. Salir.")
+  #   opcion = int(input("Escoge la opción: "))
+  #   if(opcion == 1):
+  #     name = input("Nombre: ")
+  #     age = int(input("Edad: "))
+  #     add_user(name, age)
 
-    elif(opcion == 2):
-      user = input("Ingresa el numero de usuario. ")
-      types = int(input("1. Ritmo Cardiaco.\n2. Oxigenación.\n Ingrese tipo de registro: "))
-      if (types == 1):
-        types = "Ritmo Cardiaco"
-        register = random.randint(60, 100)
-      else:
-        types = "Oxigenación"
-        register = random.randint(87, 96)
-      add_register(user, types, register)
+  #   elif(opcion == 2):
+  #     user = input("Ingresa el numero de usuario. ")
+  #     types = int(input("1. Ritmo Cardiaco.\n2. Oxigenación.\n Ingrese tipo de registro: "))
+  #     if (types == 1):
+  #       types = "Ritmo Cardiaco"
+  #       register = random.randint(60, 100)
+  #     else:
+  #       types = "Oxigenación"
+  #       register = random.randint(87, 96)
+  #     add_register(user, types, register)
+  #   else:
+  #     break
+  
+  for i in range(5000):
+    user = random.randint(1, 3)
+    types = random.randint(1, 2)
+    if (types == 1):
+      types = "Ritmo "
+      register = random.randint(60, 100)
     else:
-      break
-    a
-#  cnx.commit()
+      types = "Oxige"
+      register = random.randint(87, 96)
+    add_register(user, types, register)
+
 except mysql.connector.Error as err:
 
   if err.errno == mysql.connector.errorcode.ER_ACCESS_DENIED_ERROR:
